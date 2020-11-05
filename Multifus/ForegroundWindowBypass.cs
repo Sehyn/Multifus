3f44ef3415a4447c03af3637907c399a86e6cc10
VollRahm <39211361+VollRahm@users.noreply.github.com>
2020-11-05 18:29:01 +0100

    Added Workaround for SetForegroundWindow (#2)
    * - Added global Keyboard hook
    
    * Create .gitignore
    
    * - Added SetForegroundWindow workaround
    - Put DLLImports into Native class.

---
+++ Multifus/ForegroundWindowBypass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multifus
+{
+    
+
+    class ForegroundWindowBypass //by VollRagm
+    {
+
+		public static void Set(IntPtr hWnd)
+		{
+			uint curerntThreadId = Native.GetCurrentThreadId();	//get current ThreadId
+			IntPtr hForegroundWindow = Native.GetForegroundWindow();
+			uint activeThread = Native.GetWindowThreadProcessId(hForegroundWindow, out uint foregroundThreadId);	//get current foreground ThreadId;
+			uint windowThread = Native.GetWindowThreadProcessId(hWnd, out uint targetThreadId);	//get target window ThreadId
+
+			if (curerntThreadId != activeThread)	//If the current program is not in the foreground
+				Native.AttachThreadInput(curerntThreadId, activeThread, true);  //attach to the current foreground window -> we are now in the foreground
+            if (windowThread != curerntThreadId)    //same for the target window
+                Native.AttachThreadInput(windowThread, curerntThreadId, true);
+
+			uint lockBuffer = 0;
+			uint foo = 0;
+			//condition we need to meet from Microsoft Docs: The foreground lock time-out has expired
+			//so lets just force this
+
+			Native.SystemParametersInfo(Native.SPI_GETFOREGROUNDLOCKTIMEOUT, 0, ref lockBuffer, 0);	//store old lock time
+			Native.SystemParametersInfo(Native.SPI_SETFOREGROUNDLOCKTIMEOUT, 0, ref foo, 0);	//set lock time to 0
+			Native.LockSetForegroundWindow(2);	
+			Native.AllowSetForegroundWindow(-1);	//since we are "foreground window" we can now do this, this lets every process set the foreground window
+
+			Native.SetForegroundWindow(hWnd);	//and this
+			Native.ShowWindow(hWnd, 5);	//show the target window if its minimized
+
+			Native.SystemParametersInfo(Native.SPI_SETFOREGROUNDLOCKTIMEOUT, 0, ref lockBuffer, 0);	//reset lock time
+
+		}
+	}
+}
---
+++ Multifus/Native.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multifus
+{
+    public static class Native  //moved imports into native class   //VollRagm
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("kernel32.dll")]
+        public static extern uint GetCurrentThreadId();
+
+        [DllImport("user32.dll")]
+        public static extern bool AttachThreadInput(uint idAttach, uint idAttachTo, bool fAttach);
+
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+        public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, ref uint pvParam, uint fWinIni);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool LockSetForegroundWindow(uint uLockCode);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public const uint SPI_GETFOREGROUNDLOCKTIMEOUT = 0x2000; 
+        public const uint SPI_SETFOREGROUNDLOCKTIMEOUT = 0x2001;
+
+        [DllImport("user32.dll")]
+        public static extern bool AllowSetForegroundWindow(int dwProcessId);
+
+    }
+}
